916931a93a56f1126de8cbef0016169abc09471d
AngelaRR <82816808+AngelaRR@users.noreply.github.com>
2024-05-01 18:34:25 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -8,10 +8,12 @@ function handlerOpen() {
     let iconoMenu = document.getElementById('botonMenu');
     let menu = document.getElementById('header__menu');
     let nav = document.getElementById('header__nav');
+    let logo = document.getElementById('header__img');
 
     iconoMenu.style.display = 'none';
     menu.style.height = '100%';
     nav.style.display = 'flex';
+    logo.style.zIndex = '1'
 }
 
 
